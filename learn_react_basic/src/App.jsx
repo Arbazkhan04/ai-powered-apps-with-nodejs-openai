40f0ec56ef1bd68b046a1de9497361a2d37a7d9d
Arbaz <arbaz582021@gmail.com>
2024-07-07 06:14:25 -0400

    react-router-dom

--- learn_react_basic/src/App.jsx
+++ learn_react_basic/src/App.jsx
@@ -1,11 +1,22 @@
 
-const App = () => {
-  return (
-    <div>
-      <p>This is first JSX Element!</p>
-      <p>This is another JSX Element</p>
-    </div>
-  );
-};
+import React from 'react'
+import { BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navigation from './components/Navigation';
 
-export default App
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="contact" element={<Contact/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
